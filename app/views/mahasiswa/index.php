908147265ad208635e1c9beceb1d6441fc308144
enjoyuri <haqinhanifan@gmail.com>
2023-10-19 11:17:46 +0700

    Update index.php

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -14,7 +14,6 @@
             </button>
         </div>
     </div>
-
     <div class="row mb-3">
         <div class="col-lg-6">
             <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
