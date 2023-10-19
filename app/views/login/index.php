75a33105c079e455d09c67452dd15977f950857c
enjoyuri <haqinhanifan@gmail.com>
2023-10-19 11:34:50 +0700

    summary

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -7,34 +7,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container-fluid">
-
-        <div class="row" style="margin-top: 10%;">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
-                    <div class="login-panel panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Sign in</h3>
-                        </div>
-                        <div class="panel-body">
-                        <form action="<?= BASEURL; ?>/Log/login" method="POST" name="form" >
-                                <fieldset>
-                                    <div class="form-group">
-                                        <label>Username</label>
-                                        <input class="form-control" placeholder="username" name="username" id="username" type="text" autofocus="true" required="true">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Password</label>
-                                        <input class="form-control" placeholder="password" name="password" id="password" type="password" required="true">
-                                    </div>
-                                    <button type="submit" class="btn btn-success">Login</button>
-                                </fieldset>
-                            </form> <br>
-                            <p class="text">Don't have account? Register <a href="register.php">here!</a></p>
-                        </div>
+<div class="row">
+        <div class="col-6">
+            <div class="top-panel">
+                <div class="d-flex flex-column justify-content-center">
+                    <div class="d-flex flex-row justify-content-center">
+                        <div class="p-2 align-center"><h1>SISTEM <span style="color: aqua;">TABUNGAN</span></h1></div>
+                    </div>
+                    <div class="d-flex flex-row justify-content-center">
+                        <div class="p-2"><h1><span style="color: aqua;">SDN ERETAN</span> KULON 4</h1></div>
+                    </div>
+                    <div class="d-flex flex-row justify-content-center">
+                        <div class="p-2"><h1></div>
+                    </div>
+                    <div class="d-flex flex-row justify-content-center">
+                        <div class="p-2"></div>
+                    </div>
+                    <div class="d-flex flex-row justify-content-center">
+                        <div class="p-2"><img src="<?= BASEURL;?>/img/Kids.png" width="400" height="auto" class="img-fluid"></div>
                     </div>
                 </div>
-                <div class="col-sm-4"></div>
+            </div>
+        </div>
+        <div class="col-6 color">
+            <form name="form" method="POST" action="">
+                <fieldset>
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input class="form-control" placeholder="username" name="username" type="text" autofocus="true" required="true">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input class="form-control" placeholder="password" name="password" type="password" required="true">
+                    </div>
+                    <button type="submit" class="btn btn-success">Login</button>
+                </fieldset>
+            </form>
         </div>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
