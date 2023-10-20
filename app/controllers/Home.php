3c9d4d17e375763bd536bd67cf647b217810d708
enjoyuri <haqinhanifan@gmail.com>
2023-10-20 20:28:19 +0700

    unfinished sidebar

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -7,6 +7,7 @@ class Home extends Controller {
         $data['nama'] = $this->model('User_model')->getUser();
 
         $this->view('templates/header', $data);
+        // $this->view('templates/sidebar');
         $this->view('templates/nav');
         $this->view('home/index', $data);
         $this->view('templates/footer');
