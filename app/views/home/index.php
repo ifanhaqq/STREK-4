6b206ab6d2f2b15c703873781a9538b33379f6ad
enjoyuri <haqinhanifan@gmail.com>
2023-10-21 20:53:51 +0700

    admin landing page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,11 +1,37 @@
-<div class="container" >
-    <div class="jumbotron mt-5">
-        <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">Hello my name is <?= $data['nama'] ?></p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-        </p>
+<div class="container mt-5" >
+    <div class="row">
+        <h1 class="h1 ms-3 mb-4">Daftar Tabungan</h1>
+        <div class="col-10">
+            <table class="table border border-dark">
+                <thead class="bg-dark text-white">
+                    <tr>
+                    <th scope="col" class="border border-end border-dark w-13">No</th>
+                    <th scope="col" class="border border-end border-dark">Nama</th>
+                    <th scope="col" class="border border-end border-dark">Total Tabungan</th>
+                    <th scope="" class="border border-end border-dark col-1"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td class="border border-end border-dark">Mark</td>
+                    <td class="border border-end border-dark">1000000</td>
+                    <td class="text-center"><a href=""><i class="bi bi-plus-lg"></i></a></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td class="border border-end border-dark">Jacob</td>
+                    <td class="border border-end border-dark">1000000</td>
+                    <td class="text-center"><a href=""><i class="bi bi-plus-lg"></i></a></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td class="border border-end border-dark">Larry the Bird</td>
+                    <td class="border border-end border-dark">3000000</td>
+                    <td class="border border-end border-dark text-center"><a href=""><i class="bi bi-plus-lg"></i></a></td>
+                    </tr>
+                </tbody>
+                </table>
+        </div>
     </div>
 </div>
--- app/views/templates/nav.php
+++ app/views/templates/nav.php
@@ -1,41 +1,15 @@
-<!-- <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-success bg-strek">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="<?= BASEURL; ?>/index">STREK-4</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse d-flex" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active fw-bolder" aria-current="page" href="<?= BASEURL; ?>/index">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= BASEURL; ?>/about">Pengajuan</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="" data-bs-toggle="modal" data-bs-target="#myModal">
-                            <span class="badge bg-danger"><i class="bi bi-box-arrow-left"></i></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</div> -->
-
 <!-- Sidebar-->
 <div class="border-end bg-sidebar fw-bolder" id="sidebar-wrapper">
-    <div class="sidebar-heading border-bottom bg-light">STREK-4</div>
+    <div class="sidebar-heading border-bottom">STREK-4</div>
     <ul class="list-group mt-auto list-group-flush">
         <a href="" style="text-decoration: none"><li class="list-group-item list-group-item-action list-group-item-secondary p-3">Pengajuan</li></a>
         <a href="" style="text-decoration: none"><li class="list-group-item list-group-item-action list-group-item-secondary p-3">Mutasi</li></a>
         <a href="" style="text-decoration: none"><li class="list-group-item list-group-item-action list-group-item-secondary p-3">Registorasi</li></a>
     </ul>
     <ul class="position-absolute" style="bottom: 0">
-        <a href="" style="text-decoration: none"><li class="list-group-item list-group-item-action list-group-item-danger p-3 rounded-pill text-danger fw-bolder">Logout</li></a>
+        <div class="bg-merah rounded-pill">
+            <a href="" style="text-decoration: none"><li class="list-group-item list-group-item-action p-3 rounded-pill text-black fw-bolder">Logout</li></a>
+        </div>
     </ul>
 </div>
 <!-- Page content wrapper-->
