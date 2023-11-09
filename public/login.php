c97cc8411306a21b7898fa2573c74f549e20ece2
enjoyuri <haqinhanifan@gmail.com>
2023-11-09 11:46:42 +0700

    loginnn

--- public/login.php
+++ public/login.php
@@ -67,7 +67,6 @@
                                         <button type="submit" class="btn btn-dark mb-4 rounded-pill" name="submit">Sign in</button>
                                     </fieldset>
                                 </form>
-                                <a class="btn" data-bs-toggle="modal" data-bs-target="#tabunganModal">Launch Modal</a>
                             </div>
                         </div>
                     </div>
@@ -76,46 +75,31 @@
         </div>
     </div>
 
+
+
+
      <!-- Modal tambah data siswa -->
-<div class="modal fade" id="tabunganModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+<div class="modal hide fade" id="myModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="judulModal">Tambah Data Siswa</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form action="<?= BASEURL; ?>/tabungan/tambah" method="post">
-            <div class="mb-3 nisn-class">
-                <label for="nisn" class="form-label">NISN</label>
-                <input type="text" class="form-control" id="nisn" name="nisn">
-            </div>
-            <div class="mb-3">
-                <label for="nama" class="form-label">Nama</label>
-                <input type="text" class="form-control" id="nama" name="nama">
-            </div>
-            <div class="mb-3">
-                <label for="gender" class="form-label">Jenis Kelamin</label>
-                <select class="form-control" name="gender" id="gender">
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>
-                </select>
-            </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Tambah Data</button>
-        </form>
+      <h1 class="modal-title text-center fs-5" id="judulModal">LOGIN</h1>
       </div>
     </div>
   </div>
 </div>                              
 
 
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-<script src="js/script.js"></script>
+<script src="http://localhost/STREK-4/public/js/script.js"></script>
+<script src="http://localhost/STREK-4/public/js/scripts.js"></script>
+<script src="http://localhost/STREK-4/public/js/modal.js"></script>
 </body>
 </html>
