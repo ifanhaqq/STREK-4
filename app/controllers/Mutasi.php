5f8b0b87b23808df7591e58865f4f295bd40357c
enjoyuri <haqinhanifan@gmail.com>
2023-11-11 13:12:31 +0700

    mutasi frontend

--- app/controllers/Mutasi.php
+++ app/controllers/Mutasi.php
@@ -3,6 +3,11 @@
 class Mutasi extends Controller {
     public function index()
     {
-        
+        $data['title'] = 'Mutasi';
+
+        $this->view('templates/header', $data);
+        $this->view('templates/nav');
+        $this->view('mutasi/index', $data);
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/user/mutasi/index.php
@@ -0,0 +1,23 @@
+<div class="container mt-5">
+    <div class="row">
+        <h1 class="h1 ms-3 mb-4">Daftar Mutasi</h1>
+    </div>
+    <div class="col-7">
+    <table class="table table-striped border border-dark">
+        <thead class="bg-dark text-white">
+            <tr>
+            <th scope="col" class="border border-end border-dark w-13">Tanggal</th>
+            <th scope="col" class="border border-end border-dark">Mutasi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php ?>
+            <tr>
+            <td scope="row"><?= date("Y-m-d H:i:s") ?></td>
+            <td class="border border-end border-dark">sdf</td>
+            </tr>
+            <?php ?>
+        </tbody>
+    </table>
+    </div>
+</div>
